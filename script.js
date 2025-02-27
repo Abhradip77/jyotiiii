3db7497378359a0e79879b8a99af727b3187c436
Abhradip77 <abhra112003@gmail.com>
2025-02-27 22:09:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,66 +5,32 @@ document.addEventListener("DOMContentLoaded", function () {
         music.play().catch(error => console.log("Music playback blocked:", error));
     }
 
-    function startQuiz() {
+    document.querySelector("#promise-page button").addEventListener("click", function () {
         document.getElementById("promise-page").classList.add("hidden");
         document.getElementById("quiz-page").classList.remove("hidden");
         playMusic();
-    }
+    });
 
-    function showProposal() {
+    document.getElementById("next-button").addEventListener("click", function () {
         document.getElementById("quiz-page").classList.add("hidden");
         document.getElementById("proposal-page").classList.remove("hidden");
         playMusic();
-    }
+    });
 
-    function selectOption(option) {
-        const backgroundPic = document.getElementById("background-pic");
-        const catImg = document.getElementById("cat-img");
-        const noButton = document.getElementById("no-button");
-        const yesButton = document.getElementById("yes-button");
+    document.getElementById("yes-button").addEventListener("click", function () {
+        document.getElementById("cat-img").src = "cat-heart.gif";
+        document.body.classList.add("rainbow-flash");
+        document.getElementById("options").style.display = "none";
+    });
 
-        if (option === "yes") {
-            catImg.src = "cat-heart.gif";
-            backgroundPic.classList.remove("hidden");
-            document.body.classList.add("rainbow-flash");
-            document.getElementById("options").style.display = "none";
-        } else {
-            let noTexts = ["Still no?", "I'll die", "Don't do this", "Are you sure?"];
-            let currentNoText = noButton.innerText;
-            let newTextIndex = noTexts.indexOf(currentNoText) + 1;
-            if (newTextIndex >= noTexts.length) newTextIndex = 0;
+    document.getElementById("no-button").addEventListener("click", function () {
+        let noTexts = ["Still no?", "I'll die", "Don't do this", "Are you sure?"];
+        let currentText = this.innerText;
+        let newIndex = (noTexts.indexOf(currentText) + 1) % noTexts.length;
+        this.innerText = noTexts[newIndex];
 
-            noButton.innerText = noTexts[newTextIndex];
-            let currentSize = parseInt(window.getComputedStyle(yesButton).fontSize);
-            yesButton.style.fontSize = `${currentSize + 5}px`;
-        }
-    }
-
-    function checkAnswers() {
-        const correctAnswers = ["19 sep", "pop tates", "scorpio", "real madrid", "white and pink kurta top"];
-        let allCorrect = true;
-
-        document.querySelectorAll(".question").forEach((questionDiv, index) => {
-            const selected = questionDiv.querySelector("input:checked");
-            if (selected) {
-                if (selected.value === correctAnswers[index]) {
-                    selected.parentElement.style.backgroundColor = "green";
-                } else {
-                    selected.parentElement.style.backgroundColor = "red";
-                    allCorrect = false;
-                }
-            } else {
-                allCorrect = false;
-            }
-        });
-
-        if (allCorrect) {
-            document.getElementById("next-button").classList.remove("hidden");
-        }
-    }
-
-    window.startQuiz = startQuiz;
-    window.showProposal = showProposal;
-    window.selectOption = selectOption;
-    window.checkAnswers = checkAnswers;
+        let yesButton = document.getElementById("yes-button");
+        yesButton.style.fontSize = `${parseInt(window.getComputedStyle(yesButton).fontSize) + 5}px`;
+    });
 });
+\
