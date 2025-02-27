75e2f18169893dbe8e47bdc8e3aecc2f84246881
Abhradip77 <abhra112003@gmail.com>
2025-02-27 21:21:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,26 +66,4 @@ document.addEventListener("DOMContentLoaded", function () {
             { question: "When did we go official?", options: ["19 sep", "18 sep", "15 sep", "16 sep"] },
             { question: "Our fav spot?", options: ["si nonnas", "dailys", "high garden", "pop tates"] },
             { question: "My birth sign?", options: ["capricorn", "sagittarius", "scorpio", "cancer"] },
-            { question: "My favourite team?", options: ["united", "real madrid", "atletico madrid", "athletic club"] },
-            { question: "My fav top of yours?", options: ["black kissi one", "pink one", "yellow flower top", "white and pink kurta top"] }
-        ];
-
-        const quizContainer = document.getElementById("questions");
-        quizContainer.innerHTML = "";
-
-        quizData.forEach((q, index) => {
-            let div = document.createElement("div");
-            div.classList.add("question");
-            div.innerHTML = `<p>${index + 1}. ${q.question}</p>`;
-            q.options.forEach(opt => {
-                div.innerHTML += `<label><input type="radio" name="q${index}" value="${opt}" onchange="checkAnswers()"> ${opt}</label><br>`;
-            });
-            quizContainer.appendChild(div);
-        });
-    }
-
-    window.startQuiz = startQuiz;
-    window.showProposal = showProposal;
-    window.selectOption = selectOption;
-    window.checkAnswers = checkAnswers;
-});
+     
